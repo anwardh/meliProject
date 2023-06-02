00ef66a8e0dc539637480e4d487751115f5596da
anwardh <anwar@digitalhouse.com>
2023-06-01 23:22:11 -0300

    feat.main.go

---
+++ cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/anwardh/meliProject/cmd/server/handler"
+	"github.com/anwardh/meliProject/internal/products"
+	"github.com/gin-gonic/gin"
+)
+
+/*
+Instanciamos cada camada do domínio Products e usaremos os métodos do controlador para cada endpoint.
+*/
+
+func main() {
+	repo := products.NewRepository()
+	service := products.NewService(repo)
+	p := handler.NewProduct(service)
+
+	r := gin.Default()
+	pr := r.Group("/products")
+	pr.POST("/", p.Store())
+	pr.GET("/", p.GetAll())
+	r.Run()
+}
